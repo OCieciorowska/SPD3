893ca0c4dac622db8df31072070469eefe5e706b
Patrycja <275456@student.pwr.edu.pl>
2025-06-06 13:45:10 +0200

    testy

--- Program.cpp
+++ Program.cpp
@@ -57,7 +57,7 @@ int main() {
 
     Matrix czasy;
 
-    readFile(czasy, "test1.txt");
+    readFile(czasy, "test_daneGosia.txt");
 
     Zadanie zadanie(czasy);
 
@@ -73,12 +73,12 @@ int main() {
         std::cout << j << " ";
     std::cout << "\nCmax = " << wynik_john.second << " (Czas: " << duration.count() << " μs)" <<std::endl;
 
-    // std::cout << "\n=== Brute-force ===\n";
-    // start = std::chrono::high_resolution_clock::now();
-    // int cmax = roz.bruteForceMakespan();
-    // end = std::chrono::high_resolution_clock::now();
-    // duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    // std::cout << "Najlepsze Cmax = " << cmax << " (Czas: " << duration.count() << " μs)" << std::endl <<std::endl;
+    std::cout << "\n=== Brute-force ===\n";
+    start = std::chrono::high_resolution_clock::now();
+    int cmax = roz.bruteForceMakespan();
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Najlepsze Cmax = " << cmax << " (Czas: " << duration.count() << " μs)" << std::endl <<std::endl;
 
 
     std::cout << "\n=== NEH ===\n" << std::endl;
@@ -104,15 +104,15 @@ int main() {
     std::cout << "\nCmax = " << wynik_fneh.second << " (Czas: " << duration.count() << " μs)"<< std::endl;
 
 
-    // std::cout << "\n=== BOUND and BRANCH ===" << std::endl;
-    // start= std::chrono::high_resolution_clock::now();
-    // auto wynik_bb = roz.bound();
-    // end = std::chrono::high_resolution_clock::now();
-    // duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    // std::cout << "Kolejnosc: ";
-    // for (int zad : wynik_bb.first)
-    //     std::cout << zad << " ";
-    // std::cout << "\nCmax = " << wynik_bb.second << " (Czas: " << duration.count() << " μs)"<< std::endl;
+    std::cout << "\n=== BOUND and BRANCH ===" << std::endl;
+    start= std::chrono::high_resolution_clock::now();
+    auto wynik_bb = roz.bound();
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Kolejnosc: ";
+    for (int zad : wynik_bb.first)
+        std::cout << zad << " ";
+    std::cout << "\nCmax = " << wynik_bb.second << " (Czas: " << duration.count() << " μs)"<< std::endl;
 
     return 0;
 }
